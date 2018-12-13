db5bb6a7ca286d99a338f354d7d5c48a7d08f24c
Brandon Marquart <hammersandhair@aol.com>
2018-12-13 12:21:45 -0700

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,3 +1,26 @@
 import pygame
 import math
 import random
+
+
+class Pong: 
+  HEIGHT = 800
+  WIDTH = 600
+  
+  def __init__(self)
+  pygame.init()
+  self.screen.borderheight
+  self.screen.borderwidth.
+
+
+
+
+
+class ball(object):
+  def __init__(self, x, y, width, heigth):
+    self.x = x
+    self.y = x
+    self.width = width
+    self.heigth = heigth
+    
+  def   
